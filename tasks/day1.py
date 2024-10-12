2fe19077f84c51d6441f518cbc44423611de3cd3
SANJIKS <sanzarmaratov588@gmail.com>
2024-10-12 14:50:20 +0600

    python lessons

---
+++ tasks/day1.py
@@ -0,0 +1,43 @@
+#1
+# number = int(input('Число: '))
+
+# if number % 2 == 0:
+#     print('Четное')
+# else:
+#     print('Нечётное')
+
+#2
+# number1 = int(input('Первое число: '))
+# number2 = int(input('Второе число: '))
+
+# if number1 > number2:
+#     print(str(number1) + ' больше!')
+# elif number2 > number1:
+#     print(str(number2) + ' больше')
+# else:
+#     print('Числа равны')
+
+#3
+# temperature = int(input('Какую температуру конвертировать (C): '))
+
+# result = (temperature * 9/5) + 32
+# print('Температура в фаренгейтах: ', result)
+
+#4
+# number = int(input('Число: '))
+
+# if number < 0:
+#     print('Число отрицательное')
+# elif number > 0:
+#     print('Число положительное')
+# else:
+#     print('Ваше число - это ноль')
+
+#5
+# number = int(input('Число: '))
+# number2 = int(input('Число: '))
+
+# if number % number2 == 0:
+#     print(True)
+# else:
+#     print(False)
